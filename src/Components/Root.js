8fa26bbc28190294c9437719c66628b924072316
islammd39e <islammdrahat308@gmail.comm>
2023-01-09 21:43:48 +0600

    first commit

---
+++ src/Components/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <h2>This is Main section</h2>
+            <input type="text" placeholder="Type here" className="input input-bordered input-error w-full max-w-xs" />
+        </div>
+    );
+};
+
+export default Root;
